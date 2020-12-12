8b5bf4fe1827ce3ac597295f7dcc5b56fc78f876
shrike <shrike@shrikenoMacBook-Pro.local>
2020-12-13 00:48:19 +0900

    add:返り値のための連想配列を準備

--- CodeIgniter3/application/controllers/Posts.php
+++ CodeIgniter3/application/controllers/Posts.php
@@ -15,41 +15,40 @@ class Posts extends CI_Controller
         // $this->load->helper('url_helper');
     }
 
-    // public function index()
-    // {
-    //     // echo "hello.!!!";
-    //     //引数を指定せずに全ユーザーをモデル経由で連想配列として取得する。
-    //     $data['users'] = $this->users_model->get_users();
-    //     $data['questionnaire_results'] = $this->users_model->get_questionnaire();
-    //     $data['title'] = 'index!';
-
-    //     var_dump($data);
-    //     echo '<br>';
-    //     echo '<br>';
-    //     // var_dump('UserName:' . $data['users'][0]['line_name']);
-    //     var_dump('UserID:' . $data['users'][0]['line_id']);
-    //     echo '<br>';
-    //     echo '<br>';
-    //     var_dump('AnsswerUserId:' . $data['questionnaire_results'][0]['ansswer_user_id']);
-
-    //     $this->load->view('templates/header', $data);
-    //     $this->load->view('line/index', $data);
-    //     $this->load->view('templates/footer');
-    // }
     public function index($id = NULL) //$slug = NULL
     {
 
+        $status = array(
+            'result' => 1, 'error_info' => array('error_code' => NULL, 'error_message' => NULL),
+            'is_answer' => 0, 'is_entry' => 0, 'stamnp_result' => 0, 'is_complete' => 0
+        );
+
+        var_dump($status);
+
+        echo '<br>';
         $data['title'] = '①ステータス取得';
 
-        // echo "hello.!!!";
-        //引数を指定せずに全ユーザーをモデル経由で連想配列として取得する。
+        //引数を指定してWHERE line_id = $id のusersとアンケート情報をモデル経由で連想配列として取得する。
         $data['users'] = $this->users_model->get_users($id);
 
-        if (empty($data['users'])) { //もしも引数が空なら
-            // show_404();
-            echo "該当するユーザーがいません。";
-        } else {
-            echo '照合できました';
+        //↓取得した値に対しての正規表現による判定を後ほど実装
+        if ($id === NULL) {
+            echo '$idがNULLです';
+
+            if (empty($data['users'])) { //もしも引数が空なら
+                // show_404();
+                echo "該当するユーザーがいません。";
+                // var_dump($status);
+            } else {
+                echo '照合できました';
+                echo $data['users']['line_name'];
+                echo '<br>';
+                $status = array(
+                    'result' => 1, 'error_info' => array('error_code' => NULL, 'error_message' => NULL),
+                    'is_answer' => 1, 'is_entry' => 1, 'stamnp_result' => $data['users']['stamp_result'], 'is_complete' => $data['users']['is_complete']
+                );
+                echo json_encode($status);
+            }
         }
 
         // var_dump($data['users']['line_id']);
